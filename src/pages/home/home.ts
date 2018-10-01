e4ff6d54fa5d462f7e7d2e057222c19523684ff6
alumno Secundaria <al3201@cedi.local>
2018-10-01 08:35:22 -0500

    crear branch dibujo, luego decalarar pagina y agregar información

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HistoriaPage } from '../historia/historia';
+import { DibujoPage } from '../dibujo/dibujo';
 
 @Component({
   selector: 'page-home',
@@ -8,11 +9,16 @@ import { HistoriaPage } from '../historia/historia';
 })
 export class HomePage {
 historia = HistoriaPage;
+dibujo=DibujoPage;
   constructor(public navCtrl: NavController) {
 
   }
   clickhistoria()
   {
     this.navCtrl.push(this.historia);
+  }
+  clickdibujo()
+  {
+    this.navCtrl.push(this.dibujo);
   }
 }
